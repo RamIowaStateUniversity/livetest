d417f88bd9358eea03ec0365d8531dda696e3a9f
RamIowaStateUniversity <rramu@iastate.edu>
2016-06-17 00:37:10 -0500

    qwerty

--- Main.java
+++ Main.java
@@ -770,4 +770,16 @@ public void init(int width, int height, int levnum) {
 
         }
     }
+
+    private boolean appInstalledOrNot(String uri,PackageManager pm) {
+        boolean app_installed;
+        try {
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+            app_installed = true;
+        }
+        catch (PackageManager.NameNotFoundException e) {
+            app_installed = false;
+        }
+        return app_installed;
+    }
 }
